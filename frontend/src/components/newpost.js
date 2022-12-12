00b1852877154c3e915908b942d405c8272799c0
Susie <xu.shu1@northeastern.edu>
2022-12-12 10:58:32 -0800

    Modify newpost and postentry.

--- frontend/src/components/newpost.js
+++ frontend/src/components/newpost.js
@@ -39,54 +39,56 @@ export default function Newpost() {
   return (
     <div>
       <Header></Header>
-      <div class="container">
-        {message !== "" && (
-          <div
-            class="alert alert-warning alert-dismissible fade show"
-            role="alert"
-            id="msg"
-          >
-            <span id="msgContent">{message}</span>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="alert"
-              aria-label="Close"
-            ></button>
+      <div class="vh-100">
+        <div class="container">
+          {message !== "" && (
+            <div
+              class="alert alert-warning alert-dismissible fade show"
+              role="alert"
+              id="msg"
+            >
+              <span id="msgContent">{message}</span>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="alert"
+                aria-label="Close"
+              ></button>
+            </div>
+          )}
+          <div class="mt-4 mb-4 d-flex justify-content-between">
+            <div class="h1">Post a new experience</div>
           </div>
-        )}
-        <div class="mt-4 mb-4 d-flex justify-content-between">
-          <div class="h1">Post a new experience</div>
+          <form onSubmit={onSubmit}>
+            <div class="mb-3">
+              <label class="form-label">Title</label>
+              <input
+                name="title"
+                class="form-control"
+                placeholder="Interviewing with Twitter Inc"
+                required={true}
+                value={values.title}
+                onChange={handleChange}
+              />
+            </div>
+            <div class="mb-3">
+              <label class="form-label">Content</label>
+              <textarea
+                name="content"
+                class="form-control"
+                rows="20"
+                required={true}
+                value={values.username}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+            <div class="d-grid gap-2 mt-5 d-flex justify-content-center">
+              <button type="submit" class="btn btn-outline-danger">
+                Save
+              </button>
+            </div>
+          </form>
         </div>
-        <form onSubmit={onSubmit}>
-          <div class="mb-3">
-            <label class="form-label">Title</label>
-            <input
-              name="title"
-              class="form-control"
-              placeholder="Interviewing with Twitter Inc"
-              required={true}
-              value={values.title}
-              onChange={handleChange}
-            />
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Content</label>
-            <textarea
-              name="content"
-              class="form-control"
-              rows="20"
-              required={true}
-              value={values.username}
-              onChange={handleChange}
-            ></textarea>
-          </div>
-          <div class="d-grid gap-2 mt-5 d-flex justify-content-center">
-            <button type="submit" class="btn btn-outline-danger">
-              Save
-            </button>
-          </div>
-        </form>
       </div>
     </div>
   );
--- frontend/src/components/postentry.js
+++ frontend/src/components/postentry.js
@@ -4,10 +4,18 @@ export default function Postentry(props) {
   function handleClick(evt) {
     window.location.replace("post?id=" + props.id);
   }
+  function handleKeyPress(evt) {
+    console.log(evt.key);
+    if (evt.key === "Enter") {
+      handleClick(evt);
+    }
+  }
   return (
     <a
       onClick={handleClick}
+      onKeyPress={handleKeyPress}
       class="list-group-item list-group-item-action flex-column align-items-start"
+      tabIndex="0"
     >
       <div class="d-flex w-100 justify-content-between">
         <h5 class="mb-1">{props.title}</h5>
