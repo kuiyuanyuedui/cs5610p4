44b36c49ba3fe476dd95354ce63981e288762bb2
Susie <xu.shu1@northeastern.edu>
2022-12-12 11:10:06 -0800

    Update edit profile style

--- frontend/src/components/editprofile.js
+++ frontend/src/components/editprofile.js
@@ -57,75 +57,78 @@ export default function EditProfile() {
   return (
     <div class="container-md">
       <Header></Header>
-      <div class="h1">Edit Profile</div>
-      <form method="post" onSubmit={onSubmit}>
-        <div class="mb-3">
-          <label class="form-label">User Name</label>
-          <input
-            name="user"
-            class="form-control"
-            disabled="disabled"
-            value={profile.user}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div class="mb-3">
-          <label class="form-label">Email</label>
-          <input
-            name="email"
-            class="form-control"
-            id="username"
-            disabled="disabled"
-            value={profile.email}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div class="mb-3">
-          <label class="form-label">Program</label>
-          <input
-            name="program"
-            class="form-control"
-            value={profile.program}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div class="mb-3">
-          <label class="form-label">Location</label>
-          <input
-            name="location"
-            class="form-control"
-            value={profile.location}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div class="mb-3">
-          <label class="form-label">Offers</label>
-          <input
-            name="offers"
-            class="form-control"
-            value={profile.offers}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div class="mb-3">
-          <label class="form-label">Skills</label>
-          <input
-            name="skills"
-            class="form-control"
-            value={profile.skills}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div class="d-grid gap-2 mt-5">
-          <button
-            type="submit"
-            class="btn btn-primary"
-            style={{ backgroundColor: "#c4041d", borderColor: "#c4041d" }}
-          >
-            Save
-          </button>
-        </div>
-      </form>
+      <div class="vh-100">
+        <div class="h1">Edit Profile</div>
+        <form method="post" onSubmit={onSubmit}>
+          <div class="mb-3">
+            <label class="form-label">User Name</label>
+            <input
+              name="user"
+              class="form-control"
+              disabled="disabled"
+              value={profile.user}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Email</label>
+            <input
+              name="email"
+              class="form-control"
+              id="username"
+              disabled="disabled"
+              value={profile.email}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Program</label>
+            <input
+              name="program"
+              class="form-control"
+              value={profile.program}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Location</label>
+            <input
+              name="location"
+              class="form-control"
+              value={profile.location}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Offers</label>
+            <input
+              name="offers"
+              class="form-control"
+              value={profile.offers}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Skills</label>
+            <input
+              name="skills"
+              class="form-control"
+              value={profile.skills}
+              onChange={handleChange}
+            ></input>
+          </div>
+
+          <div class="d-flex justify-content-center">
+            <button
+              type="submit"
+              class="btn btn-primary mx-5"
+              style={{ backgroundColor: "#c4041d", borderColor: "#c4041d" }}
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
